f4ff0dfbce350122e67cabac2849d203af455463
Lucas H <jungchia.hsieh@gmail.com>
2023-03-12 12:57:25 +1100

    Replace Manager with Flask CLI
    As manager package has been depreciated.

--- manage.py
+++ manage.py
@@ -1,29 +1,29 @@
 import os
 import unittest
 
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask.cli import FlaskGroup
+from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
-migrate = Migrate(app, db)
+app = create_app()
+cli = FlaskGroup(app)
+migrate = Migrate()
 
-manager.add_command('db', MigrateCommand)
+migrate.init_app(app, db)
 
-@manager.command
+@cli.command()
 def create_tables():
     """Create database tables"""
     db.create_tables([User])
 
-@manager.command
+@cli.command()
 def runserver():
     """Run the development server"""
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
-@manager.command
+@cli.command()
 def seed():
     """Seed the database with test data."""
     user = User(username='test_user', email='test@test.com')
@@ -32,4 +32,4 @@ def seed():
     db.session.commit()
 
 if __name__ == '__main__':
-    manager.run()
+    cli()
