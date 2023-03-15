362d6055b2b8af1f2d78c59a3ec8403bfc5217bb
Lucas H <jungchia.hsieh@gmail.com>
2023-03-15 22:46:37 +1100

    Replace Placeholder Models

--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 import os
 import unittest
+import click
 
-from flask.cli import FlaskGroup
+from flask.cli import FlaskGroup, with_appcontext
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User
+from app.models import Ingredient
 
 app = create_app()
 cli = FlaskGroup(app)
@@ -15,21 +16,17 @@ migrate.init_app(app, db)
 @cli.command()
 def create_tables():
     """Create database tables"""
-    db.create_tables([User])
+    db.create_tables([Ingredient])
 
 @cli.command()
 def runserver():
     """Run the development server"""
     app.run(host='0.0.0.0', port=5000, debug=True)
 
-
 @cli.command()
 def seed():
     """Seed the database with test data."""
-    user = User(username='test_user', email='test@test.com')
-    user.set_password('password')
-    db.session.add(user)
-    db.session.commit()
+    Ingredient.seed()
 
 if __name__ == '__main__':
     cli()
