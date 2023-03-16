9efae15e1e92e0cd167b9b180a9f14ddb0654aa3
Lucas H <jungchia.hsieh@gmail.com>
2023-03-16 23:56:45 +1100

    Initialize Tables

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from peewee import Model, CharField
+from peewee import *
 from app import db
 
 
@@ -21,3 +21,48 @@ class Ingredient(BaseModel):
             for ingredient in ingredients:
                 cls.create(**ingredient)
                 
+class Recipe(BaseModel):
+    name = CharField()
+    food_type = CharField(unique=True)
+
+    @classmethod
+    def seed(cls):
+        ingredients = [
+            {'name': 'Apple', 'food_type': 'fruits'},
+            {'name': 'Milk', 'food_type': 'dairy'}
+        ]
+
+        with db.atomic():
+            for ingredient in ingredients:
+                cls.create(**ingredient)
+                
+class Menu(BaseModel):
+    menu_name = CharField(unique=True)
+    timestamp = TimestampField()
+
+    @classmethod
+    def seed(cls):
+        ingredients = [
+            {'name': 'Apple', 'food_type': 'fruits'},
+            {'name': 'Milk', 'food_type': 'dairy'}
+        ]
+
+        with db.atomic():
+            for ingredient in ingredients:
+                cls.create(**ingredient)
+
+class Review(BaseModel):
+    username = CharField()
+    review_content = TextField()
+    timestamp = TimestampField()
+
+    @classmethod
+    def seed(cls):
+        ingredients = [
+            {'name': 'Apple', 'food_type': 'fruits'},
+            {'name': 'Milk', 'food_type': 'dairy'}
+        ]
+
+        with db.atomic():
+            for ingredient in ingredients:
+                cls.create(**ingredient)
