e569f0e4e89df283509515f60b903f38a2e0b1fd
Lucas H <jungchia.hsieh@gmail.com>
2023-03-23 01:28:47 +1100

    Add Seeding Methods for Merchant and Product

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 import os
 import unittest
 import click
+import time
 
 from flask.cli import FlaskGroup, with_appcontext
 from flask_migrate import Migrate
 from app import create_app, db
 from app.models.users import User
+from app.models.products import Product
+from app.models.merchants import Merchant
 from app.models.countries import Country
-from scripts.seed import UserFactory
+
 
 app = create_app()
 cli = FlaskGroup(app)
@@ -20,12 +23,16 @@ def create_tables():
     """Create database tables"""
     db.create_tables([Country])
     db.create_tables([User])
+    db.create_tables([Merchant])
+    db.create_tables([Product])
     print("Tables created!")
 
 @cli.command()
 def drop_tables():
     """Drop database tables"""
+    db.create_tables([Product])
     db.drop_tables([User])
+    db.create_tables([Merchant])
     db.drop_tables([Country])
     print("Tables droped!")
 
@@ -38,8 +45,15 @@ def runserver():
 @click.option('--count')
 def seed(**kwargs):
     """Seed the database with test data."""
-    # Country.seed()
-    User.seed(int(kwargs['count']))
+    fake_countries = Country.seed(int(kwargs['count']))
+    
+    fake_users, fake_merchants, fake_products = [], [], []
+    for country in fake_countries:
+        tmp_user = User.seed(country_code=country)
+        fake_merchants.append(Merchant.seed(user_id=tmp_user[0], country_code=country))
+    
+    for merchant in fake_merchants:
+        fake_products.append(Product.seed(merchant_id=merchant[0]))
     print("Database seeded!")
 
 
--- scripts/seed.py
+++ scripts/seed.py
@@ -1,21 +1,51 @@
 import factory
 from factory.faker import Faker
+from factory.fuzzy import FuzzyText, FuzzyChoice
 from app.models.users import User
+from app.models.products import Product, Status
+from app.models.merchants import Merchant
+from app.models.countries import Country
 from app import db
 
 
-# Generate some fake data for the User model
+# Generate some fake data for the models
+class CountryFactory(factory.Factory):
+    class Meta:
+        model = Country
+
+    code = FuzzyText(length=2)
+    name = FuzzyText(length=20)
+
+
 class UserFactory(factory.Factory):
     class Meta:
         model = User
-        # DATABASE = db
 
-    username = factory.Sequence(lambda n: 'user{}'.format(n))
+    id = factory.Sequence(lambda n: 'user{}'.format(n))
     first_name = Faker('first_name')
     last_name = Faker('last_name')
     email = Faker('email')
     password_salt = Faker('pystr', max_chars=64)
     password_hash = Faker('pystr', max_chars=64)
     account_status = Faker('random_element', elements=('active', 'inactive'))
-    country_code = 'US' # Assuming the country code is a string
+    country_code = factory.SubFactory(CountryFactory)
 
+
+class MerchantFactory(factory.Factory):
+    class Meta:
+        model = Merchant
+    id = factory.Sequence(lambda n: n)
+    name = Faker('company')
+    country_code = factory.SubFactory(CountryFactory)
+    user_id = factory.SubFactory(UserFactory)
+
+
+class ProductFactory(factory.Factory):
+    class Meta:
+        model = Product
+    id = factory.Sequence(lambda n: n)
+    name = Faker('word')
+    price = Faker('pydecimal', left_digits=3, right_digits=2, positive=True)
+    status = FuzzyChoice(choices=[Status.OUT_OF_STOCK, Status.IN_STOCK, Status.LOW_ON_STOCK])
+    merchant_id = factory.SubFactory(MerchantFactory)
+       
