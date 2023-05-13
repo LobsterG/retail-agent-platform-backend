9ca0a3d26fb1de24a81b10404f63c37e86297cb6
Lucas H <jungchia.hsieh@gmail.com>
2023-05-14 00:16:29 +1000

    Add Business Logic to Create Order

--- app/api/order.py
+++ app/api/order.py
@@ -3,8 +3,10 @@ import logging
 from flask import jsonify, request
 from flask import Blueprint
 from app import csrf
-from app.models.orders import Order
+from app.models.orders import Order, OrderStatus, PaymentStatus
 from app.models.order_items import OrderItem
+from app.models.products import Product
+from app.models.merchant import Merchant
 from playhouse.shortcuts import model_to_dict
 from config import Config
 from peewee import DoesNotExist
@@ -29,25 +31,108 @@ def get_orders(user, jwt_token, user_id):
         logger.exception("Error getting errors:", exc_info=e)
         return jsonify({"error": "Unable to retrieve orders"}), 500
 
+
+def generate_order_id(buyer_id, payment_status):
+    order = Order.create(
+                user_id=buy_id,
+                payment_status = payment_status,
+                order_status = OrderStatus.ORDERED
+            )
+
+
+"""
+1. Create an order with the user id, order status and payment should be use default
+2. Data should contain the product, quantity and price; use this info to create one or more order_items
+3. Update product stock level if order and order item are created successfully
+4. Endpoint should check if:
+    - products' stock level is >= ordered amount
+    - ordered person is different from the merchant
+Expected data structure:
+{
+    "id": "user_id",
+    "orders": [
+        {
+            "product": "product_id",
+            "merchant": "merchant_id",
+            "quantitiy": "integer",
+            "unit_price": "float"
+        }
+    ],
+    "payment_status": "PaymentStatus enum"
+}
+"""
 @order_bp.route('/', methods=['POST'])
 @verify_jwt_token
 def create_order(user):
-    try:
+    try:        
+        # TODO: assume the data structure has already been validated at an API gateway?
         data = request.json
-        order = Order.create(**data)
-        return jsonify(order.to_dict()), 201
+
+        if data["payment_status"] not in PaymentStatus:
+            return jsonify({"error": "Invalid payment status."}), 400
+
+        # Get all the product and merchant IDs and query at once to reduct server load
+        buyer_id = data["id"]
+        product_ids = [p["product"] for p in data["orders"]]
+        purchase_products = (
+            Product.select(Product, Merchant, User)    
+            .join(Merchant)
+            .join(User)
+            .where(
+                    (Merchant.user_id != buyer_id) &
+                    (Product.id.in_(product_ids))
+                )
+        )
+        
+        if not purchase_products:
+            return jsonify({"error": "No valid products found."}), 400
+
+        # So product can be quickly accessed via ID
+        product_dict = {product.id: product for product in purchase_products}
+
+        order_id = None
+        orders = []
+        for purchase_info in data["orders"]:
+            # Check if stock level is enough to create the order and 
+            if purchase_info["product"] in product_dict and \
+                purchase_info["quantitiy"] <= product_dict[purchase_info["product"]]["stock_level"]:
+                
+                if order_id is None:
+                    order_id = generate_order_id(buyer_id, data["payment_status"])
+                
+                # Create order for item
+                orders.append(
+                    OrderItem.create(
+                        price = purchase_info["unit_price"],
+                        quantity = purchase_info["quantity"],
+                        order_id = order_id,
+                        product_id = purchase_info["product"]
+                    )
+                )
+                # Update stock level
+                product_dict[purchase_info["product"]].stock_level -= purchase_info["quantitiy"]
+                product_dict[purchase_info["product"]].save()
+
+        if not orders:
+            return jsonify({"error": "No valid orders can be created."}), 400
+
+        result = {
+            "order_id": order_id,
+            "order_items": orders.to_dict()
+        }
+        return jsonify(result), 201
     except Exception as e:
         logger.exception(f"Error creating order", exc_info=e)
         return jsonify({"error": "Unable to create order"}), 400
 
 @order_bp.route('/<userid>/<orderid>', methods=["GET"])
 @verify_jwt_token
-def get_order(user, jwt_token, ordername):
+def get_order(user, jwt_token, user_id, order_id):
     try:
         order = Order.get(Order.id == ordername)
         return jsonify(order.to_dict()), 200
     except DoesNotExist:
-        logger.debug(f"Error order not found {ordername}")
+        logger.debug(f"Error order not found {order_id}")
         return jsonify({"Error": "Order not found"}), 404
     except Exception as e:
         logger.exception(f"Error getting order", exc_info=e)
